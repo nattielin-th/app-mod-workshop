67cf07e108bc176e664995f8990b10477c2a3f2c
Natnapha EXO <natnapha_exo@HOIT202401001.local>
2026-04-08 17:54:39 +0700

    Update Config with Clean Pass

--- config.php
+++ config.php
@@ -1,16 +1,15 @@
 <?php
-// Configurazione del database
-$db_host = '34.15.141.131';
-$db_name = 'image_catalog';
-$db_user = 'appmod-phpapp-user';
-$db_pass = 'EOih|~<2dCTra9DI';
-$db_port = 3306;
-
+// Database configuration with ENV variables. Set default values as well 
+$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_name = getenv('DB_NAME') ?: 'image_catalog';
+$db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
+$db_pass = getenv('DB_PASS') ?: 'wrong_password';
+// Note getenv() is PHP 5.3 compatible
 try {
-    $pdo = new PDO("mysql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_pass);
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Erreur de connexion: " . $e->getMessage());
+    die("Errore di connessione: " . $e->getMessage());
 }
 
 session_start();
