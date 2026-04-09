27ed3bd1cb2bb944c6dcfbf6487315c512fe79dd
Natnapha EXO <natnapha_exo@HOIT202401001.local>
2026-04-09 13:43:58 +0700

    Little change to test Cloud Build

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Errore di connessione: " . $e->getMessage());
+    die("Erreur de connexion: " . $e->getMessage());
 }
 
 session_start();
